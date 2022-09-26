23fb128ae85f12caaea1fc8b256160638bcdc0a4
leeduy1 <levanduy.gv2003@gmail.com>
2022-09-26 14:10:40 +0700

    libraryPhoto

--- src/components/ItemImg/index.js
+++ src/components/ItemImg/index.js
@@ -1,19 +1,22 @@
 import React from "react";
 import classNames from "classnames/bind"
 import styles from "./ItemImg.module.scss"
-import { useDispatch } from 'react-redux';
-import { showSlideImage } from '../../redux/actions';
+// import { useDispatch } from 'react-redux';
+// import { showSlideImage } from '../../redux/actions';
 
 const cx = classNames.bind(styles)
-function ItemImg({src }) {
-  const dispatch = useDispatch();
+function ItemImg({src, index, setId, setToggle }) {
+  // const dispatch = useDispatch();
 
-  const showModal = React.useCallback(() => {
-    dispatch(showSlideImage());
-  }, [dispatch]);
+  // const showModal = React.useCallback(() => {
+  //   dispatch(showSlideImage());
+  // }, [dispatch]);
 
   return (
-    <div className={cx("box-item")} onClick={showModal}>
+    <div className={cx("box-item")} onClick={() => {
+      setId(index)
+      setToggle(true)
+    }}>
         <img src={src} alt="" />
     </div>
   )
--- src/pages/PhotoLibrary/index.js
+++ src/pages/PhotoLibrary/index.js
@@ -9,7 +9,6 @@ import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
 import { IoMdClose } from "react-icons/io";
 import ItemImg from "../../components/ItemImg";
 import { images } from "./images";
-import { Item9 } from "../../assets/img";
 import { useSelector } from 'react-redux';
 import { slideImageState$ } from '../../redux/selectors';
 
@@ -20,8 +19,10 @@ const cx = classNames.bind(styles);
 function PhotoLibrary() {
 
   
-  const { toggle } = useSelector(slideImageState$);
-
+  // const { toggle } = useSelector(slideImageState$);
+  const [id, setId] = useState(0)
+  const [toggle, setToggle] = useState(false)
+  console.log(id)
 
   return (
     <div className={cx("wrapper")}>
@@ -30,22 +31,33 @@ function PhotoLibrary() {
         <div className={cx("inner")}>
           <h2 className={cx("title")}>Thư viện ảnh</h2>
           <div className={cx("box-list")}>
-            {images.map((image) => (
-                <ItemImg src={image.src}/>
+            {images.map((image, index) => (
+                <ItemImg src={image.src} index={index} setId={setId} setToggle={setToggle}/>
             ))}
           </div>
         </div>
       </div>
       <Footer />
       <div className={cx("gallery", toggle && "show")}>
-        <IoMdClose className={cx("close")} />
+        <IoMdClose className={cx("close")} onClick={() =>setToggle(false)}/>
         <div className={cx("gallery__inner")}>
-          <img src={Item9} alt="" />
+          <img src={images[id].src} alt="" />
         </div>
-        <div className={cx("control", "prev")}>
+        <div className={cx("control", "prev")} onClick={() => {
+            setId(prev => prev - 1)
+            if(id === 0) {
+              setId(images.length - 1)
+            }
+            }
+          }>
           <BsChevronLeft />
         </div>
-        <div className={cx("control", "next")}>
+        <div className={cx("control", "next")} onClick={() =>{
+              setId(prev => prev + 1)
+              if(id === images.length - 1) {
+                setId(0)
+              }
+            }}>
           <BsChevronRight />
         </div>
       </div>
