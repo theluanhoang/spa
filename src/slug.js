38a7fcbe5bfe8b1aa48464838c5883854ab56544
LuanCodeDao <hoangtheluan2016@gmail.com>
2022-09-27 13:47:12 +0700

    upload

--- src/slug.js
+++ src/slug.js
@@ -1,4 +1,4 @@
-import { LoChanLong, NamDa } from "./assets/img"
+import { LoChanLong, NamDa, Item3, ThaiChiDocTo, CayHongSam, SeoRo, TanNhang, MoBung } from "./assets/img"
 export const slug = {
         triNam: 'dieu-tri-nam',
         tanNhang: 'dieu-tri-tan-nhang',
@@ -52,6 +52,117 @@ export const data = [
                                 'Các loại sữa rửa mặt tốt nhất giúp loại bỏ bụi bẩn và dầu thừa mà không làm mất đi độ ẩm hoàn toàn trên da. Đối với người có lỗ chân lông to do da dầu nên sử dụng sữa rửa mặt dạng gel. Da thường đến da khô có thể yên tâm dùng sữa rửa mặt dạng kem. Bất kể bạn thuộc loại da nào cũng cần tránh các loại sữa rửa mặt có chứa xà phòng hoặc chất tẩy rửa. Những loại mỹ phẩm này có thể khiến lỗ chân lông trông to hơn. Ngoài ra, bạn cần rửa mặt đúng cách để phát huy hiệu quả của sữa rửa mặt.',
                                 '* Đừng quên tẩy trang khi trang điểm',
                                 'Đi ngủ với lớp trang điểm sẽ gây hại cho làn da. Bởi mỹ phẩm có thể kết hợp với bụi bẩn, dầu và vi khuẩn làm tắc nghẽn lỗ chân lông. Đó là lý do tại sao việc tẩy trang vào buổi tối rất quan trọng, bất kể bạn có mệt hay về nhà muộn như thế nào. Để hiệu quả hơn, nữ giới có thể sử dụng sản phẩm tẩy trang trước khi rửa mặt.',
+                        ]
+                },
+                goiDau: {
+                        slug: 'goi-dau-duong-sinh',
+                        topic: 'GỘI ĐẦU DƯỠNG SINH',
+                        status: 'Gội đầu dưỡng sinh',
+                        time: '27/09/2022',
+                        image: Item3,
+                        title: ['Gội đầu dưỡng sinh và những giá trị tuyệt vời', ''],
+                        titleV2: ['Gội đầu dưỡng sinh là gì', '', ''],
+                        content: [
+                                'Gội đầu dưỡng sinh là phương pháp kết hợp giữa gội đầu bằng thảo dược với việc massage, bấm huyệt từ vùng vai trở lên giúp tuần hoàn máu, thông kinh lạc, xua tan mệt mỏi, căng thẳng. Bên cạnh đó còn giúp làm sạch da đầu và ngăn ngừa rụng tóc hiệu quả.',
+                                'THƯ GIÃN bởi với Tuyết gội đầu không chỉ là sử dụng nguyên liệu thiên nhiên chất lượng, mà còn chăm sóc khách hàng với kỹ thuật massage chuyên nghiệp được trau dồi từng ngày. Tuyết Beauty Spa hiểu rằng, khách hàng tìm đến chúng tôi là tìm đến sự thoải mái và hài lòng nhất.',
+                                'CẢI THIỆN là yếu tố tiên quyết, phục hồi da đầu bị tổn thương, mái tóc xơ yếu giúp tóc chắc khỏe từ gốc đến ngọn. Đặc biệt hơn, khi kết hợp với thủ pháp massage điêu luyện sẽ rất tốt cho sức khỏe tinh thần, khí huyết lưu thông, cân bằng cảm xúc, cải thiện giấc ngủ',
+                                '✔ Các động tác massage, bấm huyệt trong gội đầu dưỡng sinh kích thích lên phần da đầu, giúp máu huyết được lưu thông, cải thiện tình trạng đau nửa đầu, đau vai gáy, mất ngủ…',
+                                '✔ Gội đầu dưỡng sinh còn giúp tóc khỏe hơn nhờ massage, bấm huyệt, nhẹ nhàng kích thích vào chân và nang tóc giúp tóc mọc nhanh dài, nhiều và dày hơn.',
+                                '✔ Nhờ những ưu điểm tuyệt vời cho sức khỏe như vậy mà gội đầu dưỡng sinh trở thành dịch vụ được rất nhiều chị em tin tưởng và ưa chuộng. So với gội đầu thông thường, gội đầu dưỡng sinh được coi là “thần dược” giúp đánh bay mệt mỏi, thư giãn và cải thiện sức khỏe',
+                                'Để được vỗ về cả cơ thể và tinh thần bằng không gian thư giãn cùng âm nhạc, mùi hương và các tác động cơ học. Hãy tận hưởng cho mình những giờ phút thảnh thơi, tiếp thêm năng lượng cho tháng mới cùng Tuyết Beauty Spa nhé !',
+                        ]
+                },
+                thaiChi: {
+                        slug: 'thai-chi-doc-to',
+                        topic: 'THẢI CHÌ ĐỘC TỐ',
+                        status: 'Thải chì độc tố',
+                        time: '27/09/2022',
+                        image: ThaiChiDocTo,
+                        title: ['Hút Chì Thải Độc',],
+                        titleV2: ['Nguyên nhân khiến da bị nhiễm chì', '', 'Hút chì thải độc cho da là gì?'],
+                        content: [
+                                'Có rất nhiều nguyên nhân khiến da của bạn bị nhiễm chì. Môi trường, bụi bẩn, công việc, thói quen sinh hoạt là những yếu tố ảnh hưởng trực tiếp. Chất độc sẽ tích tụ dần trên da và lâu ngày sẽ gây ra các vấn đề về da. Bạn nên đi spa hút chì thải độc đều đặn 2 lần/tháng để đảm bảo da bạn luôn sạch và khỏe mạnh.',
+                                '- Môi trường ô nhiễm, khí thải từ các động cơ đốt trong như ô tô, xe máy… chứa một lượng chì không hề nhỏ. Nếu bạn sống tại các thành phố đông đúc xe cộ thì việc da của bạn bị nhiễm chì là rất cao nếu không muốn nói là chắc chắn.',
+                                '- Các loại mỹ phẩm kém chất lượng cũng tiềm ẩn khả năng chứa chì rất cao vì chì có khả năng tạo độ bám cho sản phẩm trên da của bạn. Thường là son môi, kem dưỡng, thuốc nhuộm tóc.',
+                                '- Một số đồ dùng hàng ngày cũng có thể khiến bạn có nguy cơ bị nhiễm chì như: báo giấy, thẻ cào điện thoại, túi đựng thực phẩm không rõ nguồn gốc…',
+                                'Hút chì thải độc da là một trong những phương pháp làm đẹp phổ biến hiện nay. Mục đích là loại bỏ toàn bộ lượng chì có trong da mặt, giúp bạn có làn da trắng sáng, mềm mại và mịn màng hơn. Phương pháp này sử dụng máy hút chì chuyên dụng kết hợp cùng tinh chất đặc biệt để đẩy hết chì ra bên ngoài. Hút chì thải độc không cần đụng chạm dao kéo nên rất an toàn cho người sử dụng.'
+                        ]
+                },
+                cayHongSam: {
+                        slug: 'cay-hong-sam-cang-bong',
+                        topic: 'CẤY HỒNG SÂM CĂNG BÓNG',
+                        status: 'Cấy hồng sâm căng bóng',
+                        time: '27/09/2022',
+                        image: CayHongSam,
+                        title: ['Cấy hồng sâm là gì?',],
+                        titleV2: ['Những ai nên sử dụng phương pháp cấy hồng sâm?', 'Ưu điểm của phương pháp cấy hồng sâm?', ''],
+                        content: [
+                                'Trong hồng sâm chứa thành saponin và các loại vitamin rất cao, nó có tác dụng ngăn chặn quá trình lão hóa, xóa bỏ được mọi vấn đề về da, giúp da luôn trắng sáng và căng tràn sức sống. Phương pháp này, sử dụng đầu kim nano siêu nhỏ để cấy tế bào tươi hồng sâm vào da, để nuôi dưỡng da từ sâu bên trong, mang lại hiệu quả làm đẹp tối ưu. Đặc biệt, công nghệ này phù hợp với mọi loại da, kể cả làn da nhạy cảm và mọi lứa tuổi.',
+                                'Như đã nói trên, cấy hồng sâm là phương pháp làm đẹp lành tính, không tác hại cho da hay sức khỏe. Nó phù hợp với mọi loại da, ngay cả làn da khó chiều nhất. Nhưng đặc biệt phương pháp này là giải pháp “cứu tinh” dành cho làn da:',
+                                'Da xỉn màu, nhiều tế bào chết, sần sùi, khô ráp, da có sẹo thâm, tàn dư của mụn, da bóng dầu, không đều màu, da lão hóa, mỏng yếu, dễ tổn thương, muốn cải thiện sắc diện và độ trắng của làn da.',
+                                'Da trắng sáng bật tone, tươi tắn không tỳ vết. Loại bỏ những khuyết điểm của da như thâm, nám, tàn nhang, làm đều màu da, da căng mướt, mịn màng. Làn da se khít, thu nhỏ lỗ chân lông. Làm chậm quá trình lão hóa, thúc đẩy collagen, elastin phát triển làm tăng khả năng đàn hồi và săn chắc da. Đặc biệt, nuôi dưỡng từ sâu bên trong làn da, giúp da luôn khỏe. Cấy hồng sâm không xâm lấn, không phẫu thuật và không phải nghỉ dưỡng, giúp phái đẹp giải quyết được các vấn đề về da, với ưu điểm nhẹ nhàng, thời gian thực hiện nhanh chóng, hoàn toàn không gây đau đớn hay gây kích ứng, thậm chí cấy hồng sâm được lòng ngay cả những làn da nhạy cảm nhất.',
+                        ]
+                },
+                seoRoV2: {
+                        slug: 'dieu-tri-phi-kim-seo-ro',
+                        topic: 'ĐIỀU TRỊ PHI KIM SẸO RỖ',
+                        status: 'Điều trị phi kim sẹo rỗ',
+                        time: '27/09/2022',
+                        image: SeoRo,
+                        title: ['Phi kim trị sẹo rỗ là gì? Có nên phi kim trị sẹo rỗ không?',],
+                        titleV2: ['', 'Lưu ý cần biết khi tiến hành phi kim trị sẹo rỗ', ''],
+                        content: [
+                                'Phi kim trị sẹo rỗ là một trong những biện pháp thẩm mỹ được ưa chuộng nhất hiện nay. Tuy nhiên, để mang lại hiệu quả tối đa, người dùng cần lưu ý một vài điều khi thực hiện phi kim. Hãy cùng Tuyết Beauty Spa tìm hiểu về phương pháp phi kim điều trị sẹo rỗ qua bài viết sau nhé!',
+                                'Nguyên lý hoạt động của phi kim trị sẹo rỗ như thế nào? Đầu tiên, bác sĩ sẽ dùng máy phi kim bao gồm những đầu kim nano siêu nhỏ (0.5 mm – 1mm) để phá vỡ các mô liên kết sẹo, tạo ra những vi tổn thương giả trên da. Những vi tổn thương này được thực hiện đúng độ sâu an toàn với da và ít xâm lấn hơn phương pháp lăn kim truyền thống.',
+                                'Những vi tổn thương này sẽ kích thích cơ chế tự chữa lành của cơ thể bằng cách thúc đẩy sản sinh collagen và elastin để lấp đầy những lỗ hổng do đầu kim gây ra. Nhờ đó, những vết sẹo rỗ sẽ được chữa trị một cách nhanh chóng. Phương pháp phi kim còn sử dụng kết hợp với tinh chất đặc trị, tế bào gốc để tăng hiệu quả điều trị sẹo rỗ, thúc đẩy tái tạo tế bào mới thay thế các mô da bị hư tổn do sẹo rỗ và trẻ hoá da.',
+                                'Có nên phi kim trị sẹo rỗ tại nhà không? Theo chuyên gia, bạn nên thực hiện phi kim tại những cơ sở thẩm mỹ uy tín, nơi có đầy đủ trang thiết bị và phòng trị liệu hiện đại, mọi thao tác đều đảm bảo quá trình phi kim an toàn tuyệt đối. Bởi quá trình phi kim tại nhà đôi khi không đảm bảo tối đa các yêu cầu về vệ sinh, tạo cơ hội cho vi khuẩn xâm nhập gây ra nhiễm trùng máu và nhiều biến chứng nguy hiểm khác.'
+                        ]
+                },
+                tanNhang: {
+                        slug: 'dieu-tri-tan-nhang',
+                        topic: 'ĐIỀU TRỊ TÀN NHANG',
+                        status: 'Điều trị tàn nhang',
+                        time: '27/09/2022',
+                        image: TanNhang,
+                        title: ['Nguyên nhân hình thành tàn nhang',],
+                        titleV2: ['', '', ''],
+                        content: [
+                                'Tàn nhang và sắc tố da đa dạng về màu sắc, có người màu vàng, nâu vàng, nâu sáng và phần lớn là màu nâu đen. Bản chất là sự phát triển quá mức của các sắc tố melanin và chúng thướng xuất hiện ở những người có làn da đẹp, trắng, mỏng, mịn',
+                                '- Di truyền là nguyên nhân chủ yếu.',
+                                '- Thường xuyên tiếp xúc với ánh sáng mặt trời.',
+                                '- Sự thay đổi bất thường của những hormone trong cơ thể, phản ứng phụ gây ra khi sử dụng một số chủng loại thuốc.',
+                                '- Tàn nhang có thể là biểu hiện da của một bệnh nào đó: bệnh u xơ thần kinh, bệnh lão nhi, hội chứng Moynahan và bệnh khô da sắc tố, hội chứng Peutz-Jeghers...',
+                                '- Sử dụng mỹ phẩm có chưa nhiều thành phần gây lột tẩy.',
+                        ]
+                },
+                seoRo: {
+                        slug: 'dieu-tri-seo-ro',
+                        topic: 'ĐIỀU TRỊ SẸO RỖ',
+                        status: 'Điều trị sẹo rỗ',
+                        time: '27/09/2022',
+                        image: SeoRo,
+                        title: ['Phi kim trị sẹo rỗ là gì? Có nên phi kim trị sẹo rỗ không?',],
+                        titleV2: ['', 'Lưu ý cần biết khi tiến hành phi kim trị sẹo rỗ', ''],
+                        content: [
+                                'Phi kim trị sẹo rỗ là một trong những biện pháp thẩm mỹ được ưa chuộng nhất hiện nay. Tuy nhiên, để mang lại hiệu quả tối đa, người dùng cần lưu ý một vài điều khi thực hiện phi kim. Hãy cùng Tuyết Beauty Spa tìm hiểu về phương pháp phi kim điều trị sẹo rỗ qua bài viết sau nhé!',
+                                'Nguyên lý hoạt động của phi kim trị sẹo rỗ như thế nào? Đầu tiên, bác sĩ sẽ dùng máy phi kim bao gồm những đầu kim nano siêu nhỏ (0.5 mm – 1mm) để phá vỡ các mô liên kết sẹo, tạo ra những vi tổn thương giả trên da. Những vi tổn thương này được thực hiện đúng độ sâu an toàn với da và ít xâm lấn hơn phương pháp lăn kim truyền thống.',
+                                'Những vi tổn thương này sẽ kích thích cơ chế tự chữa lành của cơ thể bằng cách thúc đẩy sản sinh collagen và elastin để lấp đầy những lỗ hổng do đầu kim gây ra. Nhờ đó, những vết sẹo rỗ sẽ được chữa trị một cách nhanh chóng. Phương pháp phi kim còn sử dụng kết hợp với tinh chất đặc trị, tế bào gốc để tăng hiệu quả điều trị sẹo rỗ, thúc đẩy tái tạo tế bào mới thay thế các mô da bị hư tổn do sẹo rỗ và trẻ hoá da.',
+                                'Có nên phi kim trị sẹo rỗ tại nhà không? Theo chuyên gia, bạn nên thực hiện phi kim tại những cơ sở thẩm mỹ uy tín, nơi có đầy đủ trang thiết bị và phòng trị liệu hiện đại, mọi thao tác đều đảm bảo quá trình phi kim an toàn tuyệt đối. Bởi quá trình phi kim tại nhà đôi khi không đảm bảo tối đa các yêu cầu về vệ sinh, tạo cơ hội cho vi khuẩn xâm nhập gây ra nhiễm trùng máu và nhiều biến chứng nguy hiểm khác.'
+                        ]
+                },
+                moBung: {
+                        slug: 'dieu-tri-seo-ro',
+                        topic: 'GIẢM MỠ VÙNG BỤNG',
+                        status: 'Giảm mỡ vùng bụng',
+                        time: '27/09/2022',
+                        image: MoBung,
+                        title: ['Phi kim trị sẹo rỗ là gì? Có nên phi kim trị sẹo rỗ không?',],
+                        titleV2: ['', 'Lưu ý cần biết khi tiến hành phi kim trị sẹo rỗ', ''],
+                        content: [
+                                'Phi kim trị sẹo rỗ là một trong những biện pháp thẩm mỹ được ưa chuộng nhất hiện nay. Tuy nhiên, để mang lại hiệu quả tối đa, người dùng cần lưu ý một vài điều khi thực hiện phi kim. Hãy cùng Tuyết Beauty Spa tìm hiểu về phương pháp phi kim điều trị sẹo rỗ qua bài viết sau nhé!',
+                                'Nguyên lý hoạt động của phi kim trị sẹo rỗ như thế nào? Đầu tiên, bác sĩ sẽ dùng máy phi kim bao gồm những đầu kim nano siêu nhỏ (0.5 mm – 1mm) để phá vỡ các mô liên kết sẹo, tạo ra những vi tổn thương giả trên da. Những vi tổn thương này được thực hiện đúng độ sâu an toàn với da và ít xâm lấn hơn phương pháp lăn kim truyền thống.',
+                                'Những vi tổn thương này sẽ kích thích cơ chế tự chữa lành của cơ thể bằng cách thúc đẩy sản sinh collagen và elastin để lấp đầy những lỗ hổng do đầu kim gây ra. Nhờ đó, những vết sẹo rỗ sẽ được chữa trị một cách nhanh chóng. Phương pháp phi kim còn sử dụng kết hợp với tinh chất đặc trị, tế bào gốc để tăng hiệu quả điều trị sẹo rỗ, thúc đẩy tái tạo tế bào mới thay thế các mô da bị hư tổn do sẹo rỗ và trẻ hoá da.',
+                                'Có nên phi kim trị sẹo rỗ tại nhà không? Theo chuyên gia, bạn nên thực hiện phi kim tại những cơ sở thẩm mỹ uy tín, nơi có đầy đủ trang thiết bị và phòng trị liệu hiện đại, mọi thao tác đều đảm bảo quá trình phi kim an toàn tuyệt đối. Bởi quá trình phi kim tại nhà đôi khi không đảm bảo tối đa các yêu cầu về vệ sinh, tạo cơ hội cho vi khuẩn xâm nhập gây ra nhiễm trùng máu và nhiều biến chứng nguy hiểm khác.'
                         ]
                 },
 
